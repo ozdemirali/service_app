3a2bd5ab25213f75400eb27a40248c71d77ea704
Ali <ozdemirali@hotmail.com>
2020-11-14 21:02:11 +0300

    Dizayn is fhnished

--- lib/widgets/showDetailForm.dart
+++ lib/widgets/showDetailForm.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
-Widget showDetailForm(BuildContext context){
+Widget showDetailForm(context) {
+  return new Scaffold(
+      body:showForm(context));
+}
+
+
+Widget showForm(BuildContext context){
   return new Container(
     padding:EdgeInsets.fromLTRB(16, 0, 16, 0),
     child: new Form(
@@ -106,6 +112,8 @@ Widget showProblem(){
   );
 }
 
+
+//Raf kayıt formunu gösteren widget
 Widget showShelf(){
   return new Padding(
     padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
